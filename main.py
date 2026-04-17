7bbc513fa6ac1aa2d4db638dd1377bf615af61e9
Soham Tambde <65997073+Sohamtam46@users.noreply.github.com>
2026-04-17 11:13:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,50 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+import requests
+from twilio.rest import Client
 
-import os
-import smtplib
-import random
-import datetime as dt
-import pandas as pd
-birthday_today = False
+account_sid = "test"
+auth_token = "test"
+client = Client(account_sid, auth_token)
 
-NAME_PLACEHOLDER = "[NAME]"
-AGE_PLACEHOLDER = "[AGE]"
+OPEN_WEATHER_API_KEY = "test"
+MY_LAT = 53.285347
+MY_LONG = -9.012642
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid":OPEN_WEATHER_API_KEY,
+    "cnt":3,
+    "units":"metric"
+}
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
 
-print("Starting program")
+will_rain = False
+weather_codes = [int(data_point["weather"][0]["id"]) for data_point in data["list"] if int(data_point["weather"][0]["id"]) < 700]
+for codes in weather_codes:
+    if codes < 700:
+        will_rain = True
 
-# getting current day details
-now = dt.datetime.now()
-present_year = now.year
-present_day = now.day
-present_month = now.month
-# getting the birthday details
-birthdays = pd.read_csv("birthdays.csv")
+if will_rain:
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="It's going to rain today. Remember to bring an umbrella",
+        to='whatsapp:+917738405806'
+    )
 
-birthdays_dict = birthdays.to_dict(orient="records")
-for people_details in birthdays_dict:
-    if people_details["month"] == present_month and people_details["day"] == present_day:
-        birthday_person_details = people_details
-        birthday_today = True
-        print("got a hit")
 
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+# for i in data["list"]:
+#     # print(i["weather"])
+#     # print(i["dt_txt"])
+#     print(f"The weather in Ballybane at {i["dt_txt"]} will be mostly {i["weather"][0]["main"]} and it would be {i["weather"][0]["description"]}.")
 
-if birthday_today:
-    with open(f"letter_templates/letter_{random.randint(1,3)}.txt") as data:
-        letter = data.read()
-        output_letter_rough =letter.replace(NAME_PLACEHOLDER,birthday_person_details["name"])
-        age = present_year - birthday_person_details["year"]
-        output_letter = output_letter_rough.replace(AGE_PLACEHOLDER,str(age))
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        print("sending email....")
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(
-                            from_addr=MY_EMAIL,
-                            to_addrs=birthday_person_details["email"],
-                            msg=f"Subject:Birthday Wishes!\n\n{output_letter}"
-                            )
+# for data_point in data["list"]:
+#     print(int(data_point["weather"][0]["id"]))
+#     if int(data_point["weather"][0]["id"]) < 700:
+#
+#         print("Bring an umbrella!")
+# message = client.messages.create(
+#     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+#     from_="+19892822992",
+#     to="+353 89 461 9023",
+# )
